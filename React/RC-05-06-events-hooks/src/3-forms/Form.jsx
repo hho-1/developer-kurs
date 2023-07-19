0e4cd67a83904a83130ca4d6d45c49431f6ce076
Habip Hakkioglu <h.hakkioglu@icloud.com>
2023-07-19 13:34:30 +0200

    class notes 07-19

--- React/RC-05-06-events-hooks/src/3-forms/Form.jsx
+++ React/RC-05-06-events-hooks/src/3-forms/Form.jsx
@@ -1,1 +1,50 @@
+import React from 'react'
+import { useState } from 'react'
 
+//! HTML'de label etiketinde belirtilen for attribute ü JSX'de htmlFor olarak tanımlanmalı.
+//! HTML'de value niteliği JSX'de defaultValue olarak tanımlanmalı.
+//! Ayrıca JSX de  javascript ifadeleri {} içinde çalıştırılabilir. Buna map, && gibi yapılarda dahil.
+
+
+const Form = () => {
+
+    const [isim, setIsim] = useState("")
+    const [pass, setPass] = useState("")
+    const [country, setCountry] = useState("")
+
+    const handleSubmit = () => {
+        alert(`name: ${isim} password: ${pass} country: ${country}`)
+    }
+
+  return (
+    <div className='container text-center mt-5'>
+        <h1>**************************************************************</h1>
+        <h1>FORM(EVENTS)</h1>
+
+        <form onSubmit={handleSubmit}>
+            <div className='mb-3'>
+                <label htmlFor="username">USERNAME: <span className='text-danger fw-bold'>{isim}</span></label>
+                <input className='form-control' type="text" id='username' onInput={(e)=>setIsim(e.target.value)}/>
+            </div>
+            <div className='mb-3'>
+                <label htmlFor="password">PASSWORD</label>
+                <input className='form-control' type="password" id='password' onChange={(e)=>setPass(e.target.value)}/>
+            </div>
+            <div className='mb-3'>
+                <label htmlFor="country">COUNTRY: <span className='text-danger fw-bold'>{country}</span></label>
+                <select className='form-select' name="country" id="country" onChange={(e)=>setCountry(e.target.value)}>
+                    <option defaultValue="countries">Countries</option>
+                    <option defaultValue="turkey">Turkey</option>
+                    <option defaultValue="germany">Germany</option>
+                    <option defaultValue="usa">USA</option>
+                </select>
+            </div>
+            <button type='submit' className='btn btn-primary btn-lg mt-4'>Submit</button>
+        </form>
+
+
+    </div>
+  )
+}
+
+export default Form
--- React/RC-05-06-events-hooks/src/App.js
+++ React/RC-05-06-events-hooks/src/App.js
@@ -3,6 +3,8 @@ import Events from './1-events-hooksIntro/Events'
 import "bootstrap/dist/css/bootstrap.min.css"
 import Hooks from './2-hooks/Hooks'
 import Counter from './2-classComponents/Counter'
+import Form from './3-forms/Form'
+import MouseKeyboard from './4-mouse-keyBoard/MouseKeyboard'
 
 const App = () => {
   return (
@@ -10,7 +12,9 @@ const App = () => {
       {/* <Events/> */}
       {/* <Hooks/> */}
       {/* <Counter/> */}
-      <Counter count={20}/>
+      {/* <Counter count={20}/> */}
+      {/* <Form/> */}
+      <MouseKeyboard/>
     </div>
   )
 }

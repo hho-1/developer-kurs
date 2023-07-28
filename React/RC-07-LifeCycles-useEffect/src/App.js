e74ee597fecc85d039e959825d2aa4472c7785b0
Habip Hakkioglu <h.hakkioglu@icloud.com>
2023-07-28 14:33:37 +0200

    class notes 28-07- friday

--- React/RC-07-LifeCycles-useEffect/src/App.js
+++ React/RC-07-LifeCycles-useEffect/src/App.js
@@ -1,10 +1,24 @@
 
+import { useState } from 'react';
 import './App.css';
+import LifeCycleMethods from './components/LifeCycleMethods';
+import Clock from './components/LifeCycleMethods';
+import "bootstrap/dist/css/bootstrap.min.css";
+import People from './components/People';
 
 function App() {
+
+  const [goster, setGoster] = useState(true)
+
   return (
     <div className="App">
-      hallo
+      <div className='containerr text-center mt-4'>
+        <button className='btn btn-danger' onClick={()=> setGoster(!goster)}>GÖSTER</button>
+  
+        {/* {goster && <LifeCycleMethods/>} */}
+        {/* {goster && <Clock/>} */}
+      </div>
+      <People/>
     </div>
   );
 }
